45f27c50c31a5b53d0a68df4b708ca2fdd3c8d6c
md_dell <jlkmh82@msn.com>
2022-02-18 00:57:43 +0900

    1-2. 포물선 운동 궤적 그리기

--- science.py
+++ science.py
@@ -1,25 +1,41 @@
 import turtle as t
+import math
+
+tm = 0.3
+ux = 0
+uy = 0
+dx = 0
+dy = 0
+g = 9.8
+velo = 0
+ang = 0
 
 def draw_pos(x,y):
-    t.clear()
+    velo = t.numinput("입력", "속도 : ", 50, 50, 100)
+    ang = math.radians(t.numinput("입력", "각도 : ", 45, 0, 360))
+
+    t.clearstamps()
+    t.hideturtle()
     t.setpos(x,y)
+    t.showturtle()
     t.stamp()
 
     hl = -(t.window_height() / 2)
 
-    tm = 0
+    ux = velo * math.cos(ang)
+    uy = velo * math.sin(ang)
+
     while True:
-        d = (9.8 * tm ** 2) / 2
-        ny = y - int(d)
-        if ny > hl:
-            t.goto(x, ny)
+        uy = uy + (-1 * g) * tm
+        dy = t.ycor() + (uy*tm) - (g * tm**2)/2
+        dx = t.xcor() + (ux*tm)
+        if dy > hl:
+            t.goto(dx, dy)
             t.stamp()
-            tm = tm + 0.3
         else:
             break
 
-t.setup(500, 600)
-t.shape("circle")
+t.setup(600, 600)
 t.shapesize(0.3, 0.3, 0)
 t.penup()
 s = t.Screen()
