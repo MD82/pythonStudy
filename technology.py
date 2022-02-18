b76ed745e7155914c956bc2d90508b80949697d0
md_dell <jlkmh82@msn.com>
2022-02-19 00:00:44 +0900

    2-2. 7세그먼트 LeD를 이용한 숫자 표시하기1

--- technology.py
+++ technology.py
@@ -1,53 +1,54 @@
 import turtle as t
 
-color_status = ["white", "blue", "red"]
-alert_status = ["정상", "주의", "화재"]
-tempc = 50
+s7seg_img = ["7s0.gif","7s1.gif","7s2.gif","7s3.gif","7s4.gif","7s5.gif","7s6.gif","7s7.gif","7s8.gif","7s9.gif","7s10.gif"]
 
-def check_fire():
-    if tempc < 80:
-        status = 0
-    elif tempc < 120:
-        status = 1
-    else:
-        status = 2
+def disp_num(k):
+    t.shape(s7seg_img[k])
+    t.stamp()
 
-    t.clear()
-    t.home()
-    t.pendown()
-    t.fillcolor(color_status[status])
-    t.begin_fill()
-    t.circle(20)
-    t.end_fill()
-    t.penup()
-    t.goto(-22, 50)
-    t.write("%s : %d"%(alert_status[status],tempc))
+def key_0():
+    disp_num(0)
+def key_1():
+    disp_num(1)
+def key_2():
+    disp_num(2)
+def key_3():
+    disp_num(3)
+def key_4():
+    disp_num(4)
+def key_5():
+    disp_num(5)
+def key_6():
+    disp_num(6)
+def key_7():
+    disp_num(7)
+def key_8():
+    disp_num(8)
+def key_9():
+    disp_num(9)
+def key_10():
+    disp_num(10)
 
-def keyUp():
-    global tempc
-    if tempc < 80:
-        tempc = tempc + 5
-    else:
-        tempc = tempc + 10
-    check_fire()
-
-def keyDown():
-    global tempc
-    if tempc <= 0:
-        tempc = 0
-    elif tempc <= 80:
-        tempc = tempc - 5
-    else:
-        tempc = tempc - 10
-    check_fire()
-
-t.setup(300, 300)
+t.setup(400, 400)
 s = t.Screen()
 t.hideturtle()
 t.speed(0)
-check_fire()
-s.onkey(keyUp, "Up")
-s.onkey(keyDown, "Down")
-s.onkey(s.bye, "q")
+
+for i in range(11):
+    s.addshape(s7seg_img[i])
+
+disp_num(10)
+
+s.onkey(key_0, "0")
+s.onkey(key_1, "1")
+s.onkey(key_2, "2")
+s.onkey(key_3, "3")
+s.onkey(key_4, "4")
+s.onkey(key_5, "5")
+s.onkey(key_6, "6")
+s.onkey(key_7, "7")
+s.onkey(key_8, "8")
+s.onkey(key_9, "9")
+s.onkey(key_10, "r")
 s.listen()
 t.mainloop() # IDE에서 실행하자마자 터틀 스크린이 꺼짐
